738580c2ef9ca613fa074d7c36887c718987a87a
coleleg <coleleg@gmail.com>
2021-11-07 20:41:42 -0500

    finalized user prompts

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,91 @@
 // Assignment code here
+
+// Prompts user to input criteria
 var promptCriteria = function() {
   window.alert("Please set the criteria for your password.");
 }
 
+// Asks for password length
 var getLength = function() {
-  var length = "";
-  while (length === "" || length < 8 || length > 128 || length === null) {
-    length = prompt("What is the desired length of your passowrd?  Select a value between 8 and 128.")
+  var length = prompt("What is the desired length of your passowrd?  Select a value between 8 and 128.")
+  if (length >+ 8 && length <= 128) {
+    alert ("You have selected " + length + " characters as your password length.");
+  }
+  else {
+    alert("You need to choose a valid option.  Try again.")
+    getLength();
   }
   
-  alert ("You have selected " + length + " characters as your password length.");
   console.log("The length of your password is " + length);
   length = parseInt(length);
   
 }
 
+// Confirms whether user would like to include numerical values
 var checkNumeric = function() {
-  var numeric = "";
-  
-  while (numeric === "" || numeric === null) {
-  numeric = prompt("Would you like to include numerical values in your password? Type 'Y' for YES and 'N' for NO.");
+  var numeric = prompt("Would you like to include numerical values in your password? Type 'Y' for YES and 'N' for NO.");
+  if (numeric === "y" || numeric === "Y") {
+    window.alert("You have selected YES to numerical values.");
+}
+  else if (numeric === "n" || numeric === "N") {
+    window.alert("You have selected NO to numerical values.");
   }
-  if (numeric.toUpperCase === "Y") {
-    alert("You have selected YES to numerical values.");
+    else {
+      window.alert("You need to choose a valid option. Try again.");
+      checkNumeric();
+    }
+
   }
-  if (numeric.toUpperCase === "N") {
-    alert("You have selected NO to numerical values.");
-  }
 
-
-  
-
-  
-  console.log("You have selected " + numeric + " to numerical values.");
-
+  // Confirms whether user would like to use lowercase letters
 var checkLowercase = function() {
-  var lowerCase = "";
-  if (lowerCase === "" || lowerCase === null) {
-    lowerCase = prompt("Would you like to include lowercase letters in your password? Type 'Y' for YES and 'N' for NO.");
+  var lowerCase = prompt("Would you like to include lowercase letters in your password? Type 'Y' for YES and 'N' for NO.");
+  if (lowerCase === "y" || lowerCase === "Y") {
+    window.alert("You have selected YES to lowercase letters.");
+}
+  else if (lowerCase === "n" || lowerCase === "N") {
+    window.alert("You have selected NO to lowercase letters.");
   }
+    else {
+      window.alert("You need to choose a valid option. Try again.");
+      checkLowercase();
+    }
 
   console.log("You have selected " + lowerCase + " to lowercase letters.")
-  lowerCase = lowerCase.toUpperCase;
+  
   
 }
 
+// Confirms whether user would like to use uppercase letters
 var checkUppercase = function() {
-  var upperCase = "";
-  while (upperCase === "" || upperCase === null) {
-    upperCase = prompt("Would you like to include uppercase letters in your password? Type 'Y' for YES and 'N' for NO.");
+  var upperCase = prompt("Would you like to include uppercase letters in your password? Type 'Y' for YES and 'N' for NO.");
+  if (upperCase === "y" || upperCase === "Y") {
+    window.alert("You have selected YES to uppercase letters.");
+}
+  else if (upperCase === "n" || upperCase === "N") {
+    window.alert("You have selected NO to uppercase letters.");
   }
+    else {
+      window.alert("You need to choose a valid option. Try again.");
+      checkUppercase();
+    }
   console.log("You have selected " + upperCase + " to uppercase letters.")
-  upperCase = upperCase.toUpperCase;
 }
 
+// Confirms whether user would like to use special characters
 var checkSpecialChar = function() {
-  var specialChar = "";
-  while (specialChar === "" || specialChar === null) {
-    specialChar = prompt("Would you like to include special characters such as '!' or '?' in your password? Type 'Y' for YES and 'N' for NO.");
+  var specialChar = prompt("Would you like to include special characters such as '!' or '?' in your password? Type 'Y' for YES and 'N' for NO.");
+  if (specialChar === "y" || specialChar === "Y") {
+    window.alert("You have selected YES to special characters.");
+}
+  else if (specialChar === "n" || specialChar === "N") {
+    window.alert("You have selected NO to special characters.");
   }
-  console.log("You have selected " + specialChar + " to special characters.")
-  specialChar = specialChar.toUpperCase;
+    else {
+      window.alert("You need to choose a valid option. Try again.");
+      checkSpecialChar();
+    }
+  console.log("You have selected " + specialChar + " to specialCharacters.")
 }
 
 // Get references to the #generate element
@@ -69,13 +94,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   promptCriteria();
-  var passwordInfo = {
-    length: getLength(),
-    numeric: checkNumeric(),
-    lowercase: checkLowercase(),
-    uppercase: checkUppercase(),
-    special: checkSpecialChar()
-  }
+  getLength();
+  checkNumeric();
+  checkLowercase();
+  checkUppercase();
+  checkSpecialChar();
+  generatePassword();
   var password = generatePassword;
   var passwordText = document.querySelector("#password");
 
@@ -87,7 +111,8 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Password Info:
-var lowerChars = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-var upperChars = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", ":", ";", "<", ">", ",", ".", "/", "?", "|"]
+function generatePassword () {
+  var pwLength = length;
+  console.log(pwLength);
+}
+
